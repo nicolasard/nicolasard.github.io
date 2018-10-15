ef595c230bf9c7069cd72ea0ba90e86f7264c686
Nicolas Ardison <nicolas.ard@gmail.com>
2018-10-15 16:37:26 -0300

    Agregando version uno de PFI

---
+++ pfi/precache-manifest.9e23680c0c2d5cb42d48c02a3670ecb0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba3e0981dae791334b971e51fccfd22a",
+    "url": "/static/media/React-logo.ba3e0981.png"
+  },
+  {
+    "revision": "11e8af26618daecab1b13cab7387ce65",
+    "url": "/static/media/JavaScript-logo.11e8af26.png"
+  },
+  {
+    "revision": "09aeab847260a350c73e2e547dce6cd9",
+    "url": "/static/media/Bootstrap-logo.09aeab84.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "387448d894264a2ef0af",
+    "url": "/static/js/main.387448d8.chunk.js"
+  },
+  {
+    "revision": "e599adc9610eee19dc08",
+    "url": "/static/js/1.e599adc9.chunk.js"
+  },
+  {
+    "revision": "387448d894264a2ef0af",
+    "url": "/static/css/main.17e1e496.chunk.css"
+  },
+  {
+    "revision": "e599adc9610eee19dc08",
+    "url": "/static/css/1.634187a8.chunk.css"
+  },
+  {
+    "revision": "0570df3850839b60ddab3eb85b718038",
+    "url": "/index.html"
+  }
+];
